9ade89c5e3f9d44591b567d5ce77abbfd8fd5a9c
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-01-12 17:33:20 -0500

    finished easy.js

---
+++ easy.js
@@ -0,0 +1,5 @@
+var fName = ["Tony", "John", "Jane"];
+var lName = ["Lee", "Smith", "Doe"];
+var age = [25, 22, 21];
+
+console.log(`Hello, my name is ${fName[1]} ${lName[1]} and I'm ${age[1]} years old.`);
