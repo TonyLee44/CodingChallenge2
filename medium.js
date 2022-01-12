6565337ce3455e62ccbe800472774cd2781b7e0b
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-01-12 17:50:49 -0500

    Finished medium.js

---
+++ medium.js
@@ -0,0 +1,29 @@
+var num = parseInt(prompt("Enter a number 1-12: "), 10);
+
+switch (num) {
+    case 1: console.log(`Number ${num} is the month of January.`);
+    break;
+    case 2: console.log(`Number ${num} is the month of February.`);
+    break;
+    case 3: console.log(`Number ${num} is the month of March.`);
+    break;
+    case 4: console.log(`Number ${num} is the month of April.`);
+    break;
+    case 5: console.log(`Number ${num} is the month of May.`);
+    break;
+    case 6: console.log(`Number ${num} is the month of June.`);
+    break;
+    case 7: console.log(`Number ${num} is the month of July.`);
+    break;
+    case 8: console.log(`Number ${num} is the month of August.`);
+    break;
+    case 9: console.log(`Number ${num} is the month of September.`);
+    break;
+    case 10: console.log(`Number ${num} is the month of October.`);
+    break;
+    case 11: console.log(`Number ${num} is the month of November.`);
+    break;
+    case 12: console.log(`Number ${num} is the month of December.`);
+    break;
+    default: console.log("You entered an invalid number!");
+}
