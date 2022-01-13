9ccbb2967fcba5e0da2a16a7797bc63fadfd4d12
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-01-13 16:29:04 -0500

    finished challenge

--- hard.js
+++ hard.js
@@ -15,12 +15,25 @@ Store Tom & Jerry’s mass and height as variables. Calculate both their BMI’s
 var names = ["Tom", "Jerry"]
 var height = [9, 10]
 var mass = [8, 45]
+let tomBmi;
+let jerryBmi;
+let isTomBmiHigher;
 
-function compare(height, mass) {
-    var bmi = mass/(height*height);
+function calculateBMI(height, mass) {
+    return mass/(height*height);
 }
 
 for (i = 0; i < names.length; i++) {
-    compare(height[i], mass[i]);
-    console.log(`${names[i]}'s height is ${height[i]} and mass is ${mass[i]}`);
+    const calculatedBMI = calculateBMI(height[i], mass[i]);
+    console.log(calculatedBMI);
+    if (names[i] === "Tom") {
+        tomBmi = calculatedBMI;
+    }
+    else {
+        jerryBmi = calculatedBMI;
+    }
 }
+
+isTomBmiHigher = (tomBmi > jerryBmi);
+
+console.log(`Is Tom's BMI higher than Jerry's? ${isTomBmiHigher}`);
